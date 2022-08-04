23f8035c30195bcdbfb3c19fea1e5c77bb115240
Woojin Lee <nijoow1127@gmail.com>
2022-08-04 18:52:13 +0900

    SEO meta 태그 설정

--- components/_common/Seo.tsx
+++ components/_common/Seo.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import metadata from "data/metadata";
-
+import { prefix } from "../../config/config";
 interface SeoProps {
   customMeta: {
     title?: string;
@@ -22,8 +22,15 @@ export default function Seo({ customMeta }: SeoProps) {
     <Head>
       <title>{headerTitle}</title>
       <link rel="shortcut icon" href="/images/favicon.ico" />
-      <meta content={meta.description} name="discription" />
-      <meta property="og:site_name" content={meta.author} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="keywords" content="title" />
+      <meta name="description" content={meta.description} />
+      <meta name="author" content={meta.author} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={headerTitle} />
+      <meta property="og:description" content={meta.description} />
+      <meta property="og:image" content={`${prefix}/images/nijoow.svg`} />
+      <meta property="og:url" content="http://www.mysite.com" />
     </Head>
   );
 }
