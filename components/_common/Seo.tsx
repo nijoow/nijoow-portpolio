f2506a3fb41fa7988c2a6463a8d752b883ef9fb8
Woojin Lee <nijoow1127@gmail.com>
2022-08-09 01:24:14 +0900

    Seo meta data 수정

--- components/_common/Seo.tsx
+++ components/_common/Seo.tsx
@@ -23,14 +23,14 @@ export default function Seo({ customMeta }: SeoProps) {
       <title>{headerTitle}</title>
       <link rel="shortcut icon" href="/images/favicon.ico" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="keywords" content="title" />
+      <meta name="keywords" content={meta.title} />
       <meta name="description" content={meta.description} />
       <meta name="author" content={meta.author} />
       <meta property="og:type" content="website" />
       <meta property="og:title" content={headerTitle} />
       <meta property="og:description" content={meta.description} />
-      <meta property="og:image" content={`${prefix}/images/nijoow.svg`} />
-      <meta property="og:url" content="http://www.mysite.com" />
+      <meta property="og:image" content={`${prefix}images/nijoow.svg`} />
+      <meta property="og:url" content="https://nijoow.github.io/" />
     </Head>
   );
 }
