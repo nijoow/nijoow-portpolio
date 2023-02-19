fd367c0a45129af058e2de855ca4db8d663e571f
Woojin Lee <nijoow1127@gmail.com>
2023-02-20 01:37:26 +0900

    💬 work 내용수정

--- pages/works/treenow.tsx
+++ pages/works/treenow.tsx
@@ -38,16 +38,21 @@ export default function TreenowPage() {
               PM 1, 프론트엔드 1, 백엔드 1, 디자이너 1
             </li>
             <li>
-              <span className="bold">[역할]</span> <br />- React.js 와
-              typescript 를 사용한 SPA 개발 <br />- React-Native 웹뷰를 사용한
-              하이브리드앱 서비스 (android, ios 배포) <br />- tailwind css를
-              활용한 UI 구현 및 반응형 웹 개발
-              <br />- 백엔드 개발자와 협업하여 REST API 연동 <br />- 백오피스
-              관리자 페이지 개발 <br />- 아임포트/KG이니시스 등 외부 API 연동을
-              통한 결제/본인인증 시스템
-              <br />- github action 을 통한 CI/CD <br />- yarn berry
-              zero-install 환경 마이그레이션 <br />- firebase cloud messaging 앱
-              푸시알림 연동
+              {" "}
+              <span className="bold">[역할]</span> <br />- 프로젝트 세팅 및 SPA
+              웹사이트 개발
+              <br />- 사용자 페이지 반응형 UI구현 및 기능 개발
+              <br />- 관리자 페이지 UI구현 및 기능 개발
+              <br />- REST API를 통한 기능 구현
+              <br />- 외부 서비스(아임포트, KG이니시스, 모두싸인, 팝빌)의
+              무통장입금 결제, 본인인증, 문서 작성 및 조회, 인증서 등록,
+              세금계산서 조회 기능 연동
+              <br />- React-Native 웹뷰를 사용한 하이브리드앱 개발 및 빌드 경험
+              <br />- 구글 플레이 스토어, 애플 앱스토어 배포 경험
+              <br />- firebase cloud messaging 앱 푸시알림 기능 구현
+              <br />- yarn berry zero-install 환경 마이그레이션을 통한 CI/CD
+              속도 개선
+              <br />- Lazy Loading, CDN 이미지 최적화로 성능 개선
             </li>
           </ul>
         </div>
