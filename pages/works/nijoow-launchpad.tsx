f0945699408c93535b947a0c3c292989b0033e84
Woojin Lee <nijoow1127@gmail.com>
2023-02-20 23:11:48 +0900

    ✏️ nijoow-launchpad 페이지 텍스트 수정

--- pages/works/nijoow-launchpad.tsx
+++ pages/works/nijoow-launchpad.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import styles from "../../styles/works/Works.module.css";
 import Work from "../../components/works/Work";
 import { BsGithub } from "react-icons/bs";
-export default function NijoowMusicPage() {
+export default function NijoowLaunchpadPage() {
   return (
     <>
       <Seo customMeta={{ title: "Treenow" }} />
@@ -27,20 +27,21 @@ export default function NijoowMusicPage() {
           <ul className={styles.infoContents}>
             <li>
               <span className="bold">nijoow-launchpad</span>
-              <br /> 음악 관련 토이프로젝트
-              <br />- 개발 진행 중
+              <br /> 전자 악기 런치패드의 형태로 피아노와 드럼을 연주할 수 있는
+              토이프로젝트 웹사이트
             </li>
             <li>
               <span className="bold">[기술 스택] </span>
-              <br />- Next.js, Typescript, Zustand, Tailwind,
+              <br />- Next.js, Typescript, Tailwind,
             </li>
-
             <li>
               <span className="bold">[기능]</span>
+              <br />- 마우스/키보드/터치 이벤트로 런치패드를 클릭할 때 마다
+              사운드 재생
             </li>
           </ul>{" "}
           <a
-            href="https://github.com/nijoow/nijoow-music"
+            href="https://github.com/nijoow/nijoow-launchpad"
             target="_blank"
             rel="noopener noreferrer"
           >
