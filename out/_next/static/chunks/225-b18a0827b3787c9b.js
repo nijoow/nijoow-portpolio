90cf34f0a68de0b49dbd6ec481ed1addef55c67a
Woojin Lee <nijoow1127@gmail.com>
2023-07-16 19:21:04 +0900

    🚀 gh-pages 배포

--- out/_next/static/chunks/225-b18a0827b3787c9b.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[225],{8225:function(n,t,e){e.d(t,{Tfp:function(){return o}});var r=e(8685);function o(n){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"polyline",attr:{points:"9 18 15 12 9 6"}}]})(n)}}}]);
