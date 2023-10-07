949ae98f05a2a1d39d72b99788c04e579c0efcb8
Woojin Lee <nijoow1127@gmail.com>
2023-10-07 17:33:33 +0900

    🚧 gh-pages 접근시 리다이렉트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import NavBar from '@/components/Navbar/NavBar'
 import Three from '@/components/Three/Three'
-import Theme from '../context/Theme'
+import Theme from '@/context/Theme'
 import { Metadata } from 'next'
 import './globals.css'
-import Recoil from '../context/Recoil'
+import Recoil from '@/context/Recoil'
+import Redirect from '@/context/Redirect'
 
 export const metadata: Metadata = {
   title: 'nijoow | portfolio',
@@ -16,28 +17,30 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className="w-full h-full" suppressHydrationWarning>
-      <body className="w-full h-full">
-        <Theme>
-          <Recoil>
-            <div className="transition-all duration-300 flex flex-col w-full min-h-screen overflow-x-hidden text-black bg-purple-50 dark:bg-gray-darker dark:text-purple-50">
-              <NavBar />
-              <div className="flex-auto w-full max-w-2xl px-3 pt-12 mx-auto">
-                <Three />
-                {children}
+    <Redirect>
+      <html lang="en" className="w-full h-full" suppressHydrationWarning>
+        <body className="w-full h-full">
+          <Theme>
+            <Recoil>
+              <div className="transition-all duration-300 flex flex-col w-full min-h-screen overflow-x-hidden text-black bg-purple-50 dark:bg-gray-darker dark:text-purple-50">
+                <NavBar />
+                <div className="flex-auto w-full max-w-2xl px-3 pt-12 mx-auto">
+                  <Three />
+                  {children}
+                </div>
+                <footer
+                  className={
+                    'text-sm py-5 w-full flex items-center justify-center'
+                  }
+                >
+                  &copy; {new Date().getFullYear()} Lee Woo Jin. All Rights
+                  Reserved.
+                </footer>
               </div>
-              <footer
-                className={
-                  'text-sm py-5 w-full flex items-center justify-center'
-                }
-              >
-                &copy; {new Date().getFullYear()} Lee Woo Jin. All Rights
-                Reserved.
-              </footer>
-            </div>
-          </Recoil>
-        </Theme>
-      </body>
-    </html>
+            </Recoil>
+          </Theme>
+        </body>
+      </html>
+    </Redirect>
   )
 }
---
+++ src/context/Redirect.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import React, { useEffect } from 'react'
+
+type Props = {
+  children: React.ReactNode
+}
+
+const Redirect = ({ children }: Props) => {
+  const router = useRouter()
+  useEffect(() => {
+    const origin = typeof window !== undefined ? window.location.origin : ''
+
+    if (origin === 'https://nijoow.github.io/')
+      router.push('https://nijoow-portfolio.vercel.app')
+  }, [])
+
+  return <>{children}</>
+}
+
+export default Redirect
