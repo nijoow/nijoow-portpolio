24374042c74d1cb0212a287df820c6645e2c0404
Woojin Lee <nijoow1127@gmail.com>
2023-10-07 18:05:52 +0900

    🚀 gh-pages 배포

--- README.md
+++
@@ -1,5 +0,0 @@
-## 🎨 개인 포트폴리오 사이트
-
-[기술 스택]
-
-- Next.js, Typescript, tailwind css
--- src/type/interface.ts
+++
@@ -1,6 +0,0 @@
-export interface Music {
-  title: string
-  artist: string
-  albumImageUrl: string
-  songUrl: string
-}
